54cf8e13fd838dc75ccb8c8e3ac3be417b495c28
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2019-12-23 12:28:19 +0600

    bump v1.4

--- kenes-widget/src/main/java/q19/kenes_widget/Constants.kt
+++ kenes-widget/src/main/java/q19/kenes_widget/Constants.kt
@@ -3,7 +3,7 @@ package q19.kenes_widget
 object Constants {
 
     private const val PROTOCOL = "https://"
-    private const val DOMAIN = "kenes.vlx.kz"
+    private const val DOMAIN = "help.post.kz"
     const val URL = "$PROTOCOL$DOMAIN/admin/widget?is_mobile=true"
 
 }
