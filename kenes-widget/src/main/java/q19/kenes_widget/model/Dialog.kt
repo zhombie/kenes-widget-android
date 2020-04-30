091cdb202feacafa80f62f433a0d8f6f5909fcd0
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2020-05-01 02:56:53 +0500

    added files for previous commit

---
+++ kenes-widget/src/main/java/q19/kenes_widget/model/Dialog.kt
@@ -0,0 +1,7 @@
+package q19.kenes_widget.model
+
+data class Dialog(
+    var operatorId: String,
+    var instance: String? = null,
+    var media: String?
+)
---
+++ kenes-widget/src/main/java/q19/kenes_widget/model/WidgetIceServer.kt
@@ -0,0 +1,8 @@
+package q19.kenes_widget.model
+
+internal data class WidgetIceServer(
+    val url: String?,
+    val username: String? = null,
+    val urls: String?,
+    val credential: String? = null
+)
